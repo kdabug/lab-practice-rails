b022f4a22968f033c2898162b094bccc1b2e422f
Mikayda Mills <mills.ma12@gmail.com>
2019-09-05 12:01:43 -0400

    add crud functionality rails

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,14 @@
 class StudentsController < ApplicationController
-   
+    before_action :set_student, only: %i[show update destroy]
+
     def index
-      @students = Student.all
+      @students = student.all
       render json: { message: "ok", students: @students }
     end
-  
+      
     def show
       begin
-        @student = Student.find(params[:id])
+        @student = student.find(params[:id])
         render json: { message: "ok", student: @student }
       rescue ActiveRecord::RecordNotFound
         render json: { message: 'no student matches that ID' }, status: 404
@@ -16,5 +17,39 @@ class StudentsController < ApplicationController
       end
     end
   
-
+    def create
+      @student = student.new(student_params)
+  
+      if @student.save
+        render json: @student, status: :created
+      else
+        render json: @student.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @student.update(student_params)
+        render json: @student
+      else
+        render json: @student.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @student.destroy
+    end
+  
+    private
+  
+    def set_student
+      @student = student.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: 'no teacher matches that ID' }, status: 404
+    end
+  
+    def student_params
+      params.require(:student).permit(:name, :description, :photo)
+    end
+  
+  
 end
